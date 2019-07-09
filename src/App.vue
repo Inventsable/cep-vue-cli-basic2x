ee51be82fe52054b163ad090d537ff2c0bdb9748
Tom Scharstein <trscharstein@gmail.com>
2019-07-08 20:13:51 -0700

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,46 @@
 <template>
   <div id="app">
+    <!-- This is the root of your panel -->
+    <!-- Content should go inside #app -->
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue CLI panel"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// You can access this App.vue file from any other component via `this.$root.$children[0]`
+// See `./components/HelloWorld.vue` for example of CSInteface and app
+
+// Create your own components and import them here
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     HelloWorld
+  },
+  data: () => ({
+    csInterface: null
+  }),
+  mounted() {
+    this.csInterface = new CSInterface();
+  },
+  methods: {
+    dispatchEvent(name, data) {
+      var event = new CSEvent(name, "APPLICATION");
+      event.data = data;
+      this.csInterface.dispatchEvent(event);
+    },
+    loadScript(path) {
+      this.csInterface.evalScript(`$.evalFile('${path}')`);
+    }
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
